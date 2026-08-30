f4dc67e5690dbe71a1dbd98e339bdfa50c103e90
samianjum87-uib <samianjum87@gmail.com>
2026-08-30 14:23:58 +0000

    3

---
+++ axis_saas/migrations/0015_remove_staffcredential_two_factor_enabled_and_more.py
@@ -0,0 +1,23 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("axis_saas", "0014_remove_totp_webauthn"),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name="staffcredential",
+            name="two_factor_enabled",
+        ),
+        migrations.RemoveField(
+            model_name="staffcredential",
+            name="two_factor_last_verified",
+        ),
+        migrations.RemoveField(
+            model_name="staffcredential",
+            name="two_factor_method",
+        ),
+    ]
